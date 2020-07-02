c73c46b270d2b14db07917746fead13080df4a02
Matt Brown <matt_d_br@hotmail.com>
2020-07-02 17:44:46 +0100

    usb-device: Start creating my own device type rather than using a CDC.
    Mbed OS have provided an implementation for a CDC but I don't want a CDC or any of the standard classes.
    
    I've started by deriving my own class from 'USBDevice'. Clearly Mbed OS don't like this because 'USBDevice.h' is in /internal/. I provided enough functions to make 'MyUSBDevice' a concrete class.
    
    Windows sees this device and Device Manager says "Unknown USB Device (Device Descriptor Request Failed)".
    
    I'm surprised that Windows does anything but it has got the error correct because I haven't implemented the device descriptor.

--- usb-device/main.cpp
+++ usb-device/main.cpp
@@ -1,16 +1,68 @@
-#include <drivers/USBCDC.h>
+#include <drivers/internal/USBDevice.h>
+#include <hal/usb/usb_phy_api.h>
 #include <rtos/ThisThread.h>
 
 #include <cstdio>
 
-USBCDC cdc;
+namespace
+{
+
+class MyUSBDevice : public USBDevice {
+public:
+    MyUSBDevice();
+    ~MyUSBDevice();
+
+protected:
+    const uint8_t *configuration_desc(uint8_t index);
+
+    void callback_state_change(DeviceState new_state);
+    void callback_request(const setup_packet_t *setup);
+    void callback_request_xfer_done(const setup_packet_t *setup, bool aborted);
+    void callback_set_configuration(uint8_t configuration);
+    void callback_set_interface(uint16_t interface, uint8_t alternate);
+};
+
+MyUSBDevice::MyUSBDevice() : USBDevice(get_usb_phy(), 0x1f00, 0x2012, 0x0001) {
+    connect();
+}
+
+MyUSBDevice::~MyUSBDevice() {
+    deinit();
+}
+
+const uint8_t *MyUSBDevice::configuration_desc(uint8_t index) {
+    return NULL;
+}
+
+void MyUSBDevice::callback_state_change(DeviceState new_state) {
+
+}
+
+void MyUSBDevice::callback_request(const setup_packet_t *setup) {
+
+}
+
+void MyUSBDevice::callback_request_xfer_done(const setup_packet_t *setup, bool aborted) {
+
+}
+
+void MyUSBDevice::callback_set_configuration(uint8_t configuration) {
+
+}
+
+void MyUSBDevice::callback_set_interface(uint16_t interface, uint8_t alternate) {
+
+}
+
+}
 
 int main() {
     printf("usb-device\n");
 
+    MyUSBDevice my_usb_device;
+
     while (1) {
-        char msg[] = "Hello world\r\n";
-        cdc.send((uint8_t *)msg, strlen(msg));
+        printf("still running...\n");
         rtos::ThisThread::sleep_for(1000);
     }
 }
