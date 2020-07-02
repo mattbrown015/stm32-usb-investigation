83366faa1c0e559a058648334eb007f2a1a9c39c
Matt Brown <matt_d_br@hotmail.com>
2020-07-02 12:46:55 +0100

    usb-device: Copied and tried the USB CDC example, it just works!
    I got this example from https://os.mbed.com/docs/mbed-os/v6.1/apis/usbcdc.html.
    
    I've just learnt what a USB communications class device (CDC) is, see https://en.wikipedia.org/wiki/USB_communications_device_class.
    
    To run the this example I'm connecting to both USB ports on the Nucleo F767.
    
    I'm using the emulator USB to get the /normal/ stdio virtual COM port.
    
    When I connect the other USB port another COM port appears and 'Hello world' appears in that COM port. Nice!

--- usb-device/main.cpp
+++ usb-device/main.cpp
@@ -1,5 +1,16 @@
+#include <drivers/USBCDC.h>
+#include <rtos/ThisThread.h>
+
 #include <cstdio>
+
+USBCDC cdc;
 
 int main() {
     printf("usb-device\n");
+
+    while (1) {
+        char msg[] = "Hello world\r\n";
+        cdc.send((uint8_t *)msg, strlen(msg));
+        rtos::ThisThread::sleep_for(1000);
+    }
 }
