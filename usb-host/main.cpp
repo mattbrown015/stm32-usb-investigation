1d6ad8a1bdc9baeac951d8cf638bef5427e8793a
Matt Brown <matt_d_br@hotmail.com>
2020-07-02 14:51:05 +0100

    usb-host: Initialise libusb.
    My plan is to use libusb, WinUSB or something to communicate with and test the USB device.
    
    I used libusb sometime ago and hence have started by reminding myself what it is.
    
    So far I've been using mingw64 and MSYS64 because it felt like an easy way to install a compiler and libusb.
    
    I already had mingw64 installed and g++ was available.
    
    I installed libusb, in mingw64, using 'pacman -S mingw-w64-x86_64-libusb'.

--- usb-host/main.cpp
+++ usb-host/main.cpp
@@ -1,6 +1,19 @@
+#include <libusb-1.0/libusb.h>
+
 #include <cstdio>
 
 int main() {
     printf("usb-host\n");
-}
 
+    const auto r = libusb_init(NULL);
+    if (r < 0) {
+        printf("libusb_init failed %d\n", r);
+        return 1;
+    } else {
+        printf("libusb_init success\n");
+    }
+
+    libusb_exit(NULL);
+
+    return 0;
+}
