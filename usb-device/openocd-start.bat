a0465aa68b31f5ebf639ca718308db1c9ace6bbb
Matt Brown <matt_d_br@hotmail.com>
2020-07-05 20:49:56 +0100

    usb-device: Only specify the OpenOCD board file.
    This batch file was based on something I'd done a couple of years ago.
    
    Clearly back then I felt it necessary to specify the interface configuration and board.
    
    I've tried again and it doesn't appear necessary now.

--- usb-device/openocd-start.bat
+++ usb-device/openocd-start.bat
@@ -2,4 +2,4 @@ rem A binary distribution of OpenOCD can be found here https://xpack.github.io/o
 rem This website includes full instructions, I followed the 'Manual install' described here https://xpack.github.io/openocd/install/
 rem Download the latest version from the release page https://xpack.github.io/openocd/releases/
 
-C:\Users\matthewb\AppData\Roaming\xPacks\OpenOCD\0.10.0-14\bin\openocd.exe --file interface\stlink.cfg --file board\st_nucleo_f7.cfg
+C:\Users\matthewb\AppData\Roaming\xPacks\OpenOCD\0.10.0-14\bin\openocd.exe --file board\st_nucleo_f7.cfg
