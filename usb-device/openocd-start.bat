e2cca837eb6ae7aecdad4b602e1270409db9bcbc
Matt Brown <matt_d_br@hotmail.com>
2020-07-05 20:42:31 +0100

    usb-device: Corrected the description of where to find the Windows distribution of OpenOCD.
    I found the correct/latest website for the Windows distribution of OpenOCD.
    
    It described where I should have installed OpenOCD so I moved it.
    
    Now the path doesn't contain spaces I removed the quotation marks.
    
    It also seems it is unnecessary to specify the full path for the configuration files.

--- usb-device/openocd-start.bat
+++ usb-device/openocd-start.bat
@@ -1,7 +1,5 @@
-rem Download https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-win32-x64.zip
-rem Unzip into ""C:\Program Files\GNU MCU Eclipse\OpenOCD\"
-rem The prebuilt binaries for OpenOCD on Windows have moved around a bit and this directory name might not be entirely
-rem appropriate but I had a previous version installed here.
-rem Connect NUCLEO_F767ZI in the /normal/ way using the USB and the on-board ST-LINK/V2
+rem A binary distribution of OpenOCD can be found here https://xpack.github.io/openocd/
+rem This website includes full instructions, I followed the 'Manual install' described here https://xpack.github.io/openocd/install/
+rem Download the latest version from the release page https://xpack.github.io/openocd/releases/
 
-"C:\Program Files\GNU MCU Eclipse\OpenOCD\xpack-openocd-0.10.0-14\bin\openocd.exe" --file "C:\Program Files\GNU MCU Eclipse\OpenOCD\xpack-openocd-0.10.0-14\scripts\interface\stlink.cfg" --file "C:\Program Files\GNU MCU Eclipse\OpenOCD\xpack-openocd-0.10.0-14\scripts\board\st_nucleo_f7.cfg"
+C:\Users\matthewb\AppData\Roaming\xPacks\OpenOCD\0.10.0-14\bin\openocd.exe --file interface\stlink.cfg --file board\st_nucleo_f7.cfg
