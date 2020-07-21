1abec57c608ce86ac5ef2914976248939e1affde
Matt Brown <matt_d_br@hotmail.com>
2020-07-21 09:56:47 +0100

    usb-device: Call 'HAL_PCD_Init' with configuration appropriate for USB HS and the STM32F723.
    This configuration is a copy of what is done in 'USBPhyHw::init' and 'STM32Cube_FW_F7_V1.16.0/Projects/STM32F723E-Discovery/Applications/USB_Device/HID_Standalone/Src/usbd_conf.c'.
    
    'USBPhyHw::init' sets 'dev_endpoints' to 8 which I think is an indication that 'USBPhyHw::init' is more generic than 'usbd_conf.c'. It could also be an indication that it is wrong!
    
    Hence I've paid more attention to 'usbd_conf.c' although at this stage it probably doesn't make much difference.
    
    Calling 'HAL_PCD_Init' sets plenty of registers but it doesn't cause any USB activity.
    
    I don't think anything happens on the USB until 'OTG_DCTL.SDIS' is cleared.

--- usb-device/evk-usb-device-hal.cpp
+++ usb-device/evk-usb-device-hal.cpp
@@ -1,14 +1,51 @@
 #include "evk-usb-device-hal.h"
 
+#include <platform/mbed_assert.h>
 #include <targets/TARGET_STM/TARGET_STM32F7/STM32Cube_FW/STM32F7xx_HAL_Driver/stm32f7xx_hal.h>
 
 namespace evk_usb_device_hal
 {
 
+PCD_HandleTypeDef hpcd = {
+    .Instance = USB_OTG_HS,
+    .Init = {
+        .dev_endpoints = 9,  // STM32F72x has 1 bidirectional control endpoint0 and 8 HS endpoints configurable to support bulk, interrupt or isochronous transfers
+        .Host_channels = 0,  // STM32F72x has 16 HS host channels but setting to 0 as not using host mode
+        .speed = PCD_SPEED_HIGH,
+        // Disable embedded DMA initially although need to find out how to use DMA.
+        // HAL_PCD_Init sets this to DISABLE depending on the value of OTG_CID i.e. it checks if this is a HS device
+        .dma_enable = DISABLE,
+        .ep0_mps = USB_OTG_MAX_EP0_SIZE,
+        .phy_itface = USB_OTG_HS_EMBEDDED_PHY,
+        .Sof_enable = DISABLE,
+        .low_power_enable = DISABLE,
+        .lpm_enable = DISABLE,
+        .battery_charging_enable = DISABLE,
+        .vbus_sensing_enable = ENABLE,
+        .use_dedicated_ep1 = DISABLE,
+        .use_external_vbus = DISABLE
+    },
+    .USB_Address = 0,  // HAL_PCD_Init set this to 0
+    // HAL_PCD_Init initialises the IN_ep and OUT_ep upto the number specified in in dev_endpoints.
+    .IN_ep = { 0 },
+    .OUT_ep = { 0 },
+    .Lock = HAL_UNLOCKED,
+    .State = HAL_PCD_STATE_RESET,
+    .ErrorCode = 0,
+    .Setup = { 0 },
+    .LPM_State = LPM_L0,
+    .BESL = 0,
+    .lpm_active = DISABLE,
+    .pData = nullptr,
+};
+
 void init() {
     __HAL_RCC_OTGPHYC_CLK_ENABLE();
     __HAL_RCC_USB_OTG_HS_CLK_ENABLE();
     __HAL_RCC_USB_OTG_HS_ULPI_CLK_ENABLE();
+
+    MBED_UNUSED const HAL_StatusTypeDef ret = HAL_PCD_Init(&hpcd);
+    MBED_ASSERT(ret == HAL_OK);
 }
 
 }
