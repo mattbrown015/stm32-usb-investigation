5049ac90fcd3f9f7670e68c35bdaa427fa34029a
Matt Brown <matt_d_br@hotmail.com>
2020-07-21 09:29:14 +0100

    usb-device: Enable the USB HS related peripherals.
    This is based on what happens in 'USBPhyHw::init'.
    
    I'm not attempting to support anything other than HS.
    
    Just enabling the peripherals isn't going to do much.

--- usb-device/evk-usb-device-hal.cpp
+++ usb-device/evk-usb-device-hal.cpp
@@ -1,9 +1,14 @@
 #include "evk-usb-device-hal.h"
 
+#include <targets/TARGET_STM/TARGET_STM32F7/STM32Cube_FW/STM32F7xx_HAL_Driver/stm32f7xx_hal.h>
+
 namespace evk_usb_device_hal
 {
 
 void init() {
+    __HAL_RCC_OTGPHYC_CLK_ENABLE();
+    __HAL_RCC_USB_OTG_HS_CLK_ENABLE();
+    __HAL_RCC_USB_OTG_HS_ULPI_CLK_ENABLE();
 }
 
 }
