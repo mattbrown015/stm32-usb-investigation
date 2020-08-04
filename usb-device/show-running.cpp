ab8303be92360837c9f00bfff5285206fb896932
Matt Brown <matt_d_br@hotmail.com>
2020-08-04 14:27:56 +0100

    usb-device: Use the STM32 HAL and LL drivers for simple GPIO control.
    Using the STM32 LL makes it possible to use 'LL_GPIO_TogglePin' which is such a nice function I couldn't resist ditching 'mbed::DigitalOut'.
    
    Of course this means it is no longer platform independent but that boat sailed with the USB and SPI modules.

--- usb-device/show-running.cpp
+++ usb-device/show-running.cpp
@@ -1,8 +1,8 @@
 #include "show-running.h"
 
-#include <drivers/DigitalOut.h>
 #include <rtos/ThisThread.h>
 #include <rtos/Thread.h>
+#include <targets/TARGET_STM/TARGET_STM32F7/STM32Cube_FW/STM32F7xx_HAL_Driver/stm32f7xx_ll_gpio.h>
 
 namespace show_running
 {
@@ -10,17 +10,30 @@ namespace show_running
 namespace
 {
 
-mbed::DigitalOut green_led(LED1);
-
 const size_t stack_size = 512; // Arbitrarily /small/ stack
 MBED_ALIGN(8) unsigned char stack[stack_size];
 rtos::Thread thread(osPriorityLow, sizeof(stack), stack, "show_running");
 
+// Use green LED to indicate thread running.
+void led_init() {
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    // PB1, SYS_LD_USER2, green
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinSpeed(GPIOB, LL_GPIO_PIN_1, LL_GPIO_SPEED_FREQ_LOW);
+    LL_GPIO_SetPinOutputType(GPIOB, LL_GPIO_PIN_1, LL_GPIO_OUTPUT_PUSHPULL);
+    LL_GPIO_SetPinPull(GPIOB, LL_GPIO_PIN_1, LL_GPIO_PULL_NO);
+    LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_1);
+}
+
+
 void show_running() {
     using namespace std::chrono_literals;
 
+    led_init();
+
     while (1) {
-        green_led = !green_led;
+        LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_1);
 
         rtos::ThisThread::sleep_for(1s);
     }
